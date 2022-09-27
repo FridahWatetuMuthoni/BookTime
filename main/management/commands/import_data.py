a8b81392a0a0bcece829a3a70170d02a692f7a88
FridahWatetu <fridahwatetumuthoni@gmail.com>
2022-09-27 16:53:03 +0300

    success

--- main/management/commands/import_data.py
+++ main/management/commands/import_data.py
@@ -1,8 +1,18 @@
+from collections import Counter
+import csv
+import os.path
+from django.core.files.images import ImageFile
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import slugify
+from main import models
 
 
 class Command(BaseCommand):
     help = 'Import products in BookTime'
+
+    def add_arguments(self, parser):
+        parser.add_argument("csvfile", type=open)
+        parser.add_argument("image_basedir", type=str)
 
     def handle(self, *args, **options):
         self.stdout.write("Importing products")
